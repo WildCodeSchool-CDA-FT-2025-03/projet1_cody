e26373ab03a1f46d334fbf1be214cab543f223e3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 15:51:56 +0100

    Création des composants FooterRoot

---
+++ client/src/components/FooterRoot/FooterRoot.tsx
@@ -0,0 +1,13 @@
+import style from './FooterRoot.module.css';
+
+function FooterRoot() {
+    return (
+        <div className={style.FooterRoot}>
+            <h1>
+                Je suis dans la page Footer
+            </h1>
+        </div>
+    )
+}
+
+export default FooterRoot;
