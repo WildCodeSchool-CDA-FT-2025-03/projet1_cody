55a2eb271abb7bdf2d522bd1cb1801b0f71081d6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 15:48:06 +0100

    Création des fichiers AccountRoot

---
+++ client/src/components/AccountRoot/AccountRoot.tsx
@@ -0,0 +1,13 @@
+import style from './AccountRoot.module.css';
+
+function AccountRoot() {
+    return (
+        <div className={style.AccountRoot}>
+            <h1>
+                Je suis dans la page compte
+            </h1>
+        </div>
+    )
+}
+
+export default AccountRoot;
