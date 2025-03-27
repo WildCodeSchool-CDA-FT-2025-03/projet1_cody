415a9bcbd9002e49f981fda87e1dd0d9fbe7bdb4
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 15:49:22 +0100

    Création des fichiers AdminRoot

---
+++ client/src/components/AdminRoot/AdminRoot.tsx
@@ -0,0 +1,13 @@
+import style from './AdminRoot.module.css';
+
+function AdminRoot() {
+    return (
+        <div className={style.AdminRoot}>
+            <h1>
+                Je suis dans la page admin
+            </h1>
+        </div>
+    )
+}
+
+export default AdminRoot;
