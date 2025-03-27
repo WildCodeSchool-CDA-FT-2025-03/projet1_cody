c77181c62e8baad8575031532a218f10aab697a3
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 15:52:37 +0100

    Création des composants GameRoot

---
+++ client/src/components/GameRoote/GameRoot.tsx
@@ -0,0 +1,13 @@
+import style from './GameRoot.module.css';
+
+function GameRoot() {
+    return (
+        <div className={style.GameRoot}>
+            <h1>
+                Je suis dans la page Game
+            </h1>
+        </div>
+    )
+}
+
+export default GameRoot;
