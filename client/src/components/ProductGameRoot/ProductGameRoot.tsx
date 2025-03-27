f1205244573756deffd20e26ae7461eb1674b838
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 16:23:15 +0100

    Création des composants ProductGameRoot

---
+++ client/src/components/ProductGameRoot/ProductGameRoot.tsx
@@ -0,0 +1,13 @@
+import style from './ProductGameRootRoot.module.css';
+
+function ProductGameRootRoot() {
+    return (
+        <div className={style.ProductGameRootRoot}>
+            <h1>
+                Je suis dans la page ProductGameRoot
+            </h1>
+        </div>
+    )
+}
+
+export default ProductGameRootRoot;
