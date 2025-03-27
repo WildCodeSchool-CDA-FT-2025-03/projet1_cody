24ab5aa90aff066790e7b0955ebc8571bad958a6
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 16:09:03 +0100

    Création des composants MovieRoot

---
+++ client/src/components/MovieRoot/MovieRoot.tsx
@@ -0,0 +1,13 @@
+import style from './MovieRoot.module.css';
+
+function MovieRoot() {
+    return (
+        <div className={style.MovieRoot}>
+            <h1>
+                Je suis dans la page Movie
+            </h1>
+        </div>
+    )
+}
+
+export default MovieRoot;
