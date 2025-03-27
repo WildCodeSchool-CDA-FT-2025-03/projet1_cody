a6c4484ad19323c4d4e0618a6c2c02b0297dd1cf
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 16:15:46 +0100

    Création des composants NavRoot

---
+++ client/src/components/NavRoot/NavRoot.tsx
@@ -0,0 +1,13 @@
+import style from './NavRoot.module.css';
+
+function NavRoot() {
+    return (
+        <div className={style.NavRoot}>
+            <h1>
+                Je suis dans la page Nav
+            </h1>
+        </div>
+    )
+}
+
+export default NavRoot;
