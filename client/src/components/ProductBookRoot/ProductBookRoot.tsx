2e05ca77f85f33fef8df423a6c3d141eb758055b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 16:20:28 +0100

    Création des composants ProductBookRoot

---
+++ client/src/components/ProductBookRoot/ProductBookRoot.tsx
@@ -0,0 +1,13 @@
+import style from './ProductBookRootRoot.module.css';
+
+function ProductBookRootRoot() {
+    return (
+        <div className={style.ProductBookRootRoot}>
+            <h1>
+                Je suis dans la page ProductBookRoot
+            </h1>
+        </div>
+    )
+}
+
+export default ProductBookRootRoot;
