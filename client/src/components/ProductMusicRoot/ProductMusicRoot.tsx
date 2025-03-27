c9deb0d591304ccb78a8376fbbcf52955133153b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-27 16:26:00 +0100

    Création des composants MusicRoot

---
+++ client/src/components/ProductMusicRoot/ProductMusicRoot.tsx
@@ -0,0 +1,13 @@
+import style from './ProductMusicRootRoot.module.css';
+
+function ProductMusicRootRoot() {
+    return (
+        <div className={style.ProductMusicRootRoot}>
+            <h1>
+                Je suis dans la page ProductMusicRoot
+            </h1>
+        </div>
+    )
+}
+
+export default ProductMusicRootRoot;
