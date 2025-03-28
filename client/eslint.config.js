e85789842d6c87c01cdeb3c246bdf626e92a98a5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-28 15:13:50 +0100

    test

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,32 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      sourceType: 'module',
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'semi': ['error', 'always'],
       'quotes': ['error', 'double'],
       'indent': ['error', 2],
       'no-console': ['error', { allow: ['warn', 'error'] }],
+      'import/no-unresolved': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+];
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-// lint-staged.config.js
-export default {
-    'client/**/*.{ts,tsx}': ['npm --prefix client run lint:fix'],
-  };
-  
