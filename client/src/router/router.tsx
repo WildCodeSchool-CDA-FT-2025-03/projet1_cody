e48cf7ae430cf35849822de27d597f16ff5c9eca
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-28 14:22:42 +0100

    Rectification du fichier router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -4,19 +4,19 @@ import { ReactNode } from "react";
 
 import HomePage from "../pages/HomePage/HomePage";
 
-import AccountPage from "../pages/AccountPage/AccountPage";
-import AdminPage from "../pages/AdminPage/AdminPage";
+// import AccountPage from "../pages/AccountPage/AccountPage";
+// import AdminPage from "../pages/AdminPage/AdminPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 import BookPage from "../pages/BookPage/BookPage";
-import GamePage from "../pages/GamePage/GamePage";
-import MoviePage from "../pages/MoviePage/MoviePage";
-import MusicPage from "../pages/MusicPage/MusicPage";
+// import GamePage from "../pages/GamePage/GamePage";
+// import MoviePage from "../pages/MoviePage/MoviePage";
+// import MusicPage from "../pages/MusicPage/MusicPage";
 
-import ProductBookPage from "../pages/ProductBookPage/ProductBookPage";
-import ProductGamePage from "../pages/ProductGamePage/ProductGamePage";
-import ProductMoviePage from "../pages/ProductMoviePage/ProductMoviePage";
-import ProductMusicPage from "../pages/ProductMusicPage/ProductMusicPage";
+// import ProductBookPage from "../pages/ProductBookPage/ProductBookPage";
+// import ProductGamePage from "../pages/ProductGamePage/ProductGamePage";
+// import ProductMoviePage from "../pages/ProductMoviePage/ProductMoviePage";
+// import ProductMusicPage from "../pages/ProductMusicPage/ProductMusicPage";
 
 interface RouterClientType {
   path: string;
@@ -29,16 +29,6 @@ const routerClient: RouterClientType[] = [
     path: "/",
     element: <HomePage />,
   },
-  /* {routerClient[1].path} */
-  {
-    path: "/account",
-    element: <AccountPage />,
-  },
-  /* {routerClient[2].path} */
-  {
-    path: "/admin",
-    element: <AdminPage />,
-  },
   /* {routerClient[3].path} */
   {
     path: "/not-found",
@@ -48,41 +38,6 @@ const routerClient: RouterClientType[] = [
   {
     path: "/book",
     element: <BookPage />,
-  },
-  /* {routerClient[5].path} */
-  {
-    path: "/game",
-    element: <GamePage />,
-  },
-  /* {routerClient[6].path} */
-  {
-    path: "/movie",
-    element: <MoviePage />,
-  },
-  /* {routerClient[7].path} */
-  {
-    path: "/music",
-    element: <MusicPage />,
-  },
-  /* {routerClient[8].path} */
-  {
-    path: "/product-book",
-    element: <ProductBookPage />,
-  },
-  /* {routerClient[9].path} */
-  {
-    path: "/product-game",
-    element: <ProductGamePage />,
-  },
-  /* {routerClient[10].path} */
-  {
-    path: "/product-movie",
-    element: <ProductMoviePage />,
-  },
-  /* {routerClient[11].path} */
-  {
-    path: "/product-music",
-    element: <ProductMusicPage />,
   },
 ];
 
