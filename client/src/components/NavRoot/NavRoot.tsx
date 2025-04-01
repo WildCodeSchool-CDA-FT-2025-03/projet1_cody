87472f5f2ba74dc88e02ae485e0acd07d0c71f9f
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-01 17:41:23 +0200

    Modification de la balise Nav

--- client/src/components/NavRoot/NavRoot.tsx
+++ client/src/components/NavRoot/NavRoot.tsx
@@ -2,11 +2,11 @@ import style from "./NavRoot.module.css";
 
 function NavRoot() {
   return (
-    <div className={style.NavRoot}>
+    <nav className={style.NavRoot}>
       <h1>
         Je suis dans la page Nav
       </h1>
-    </div>
+    </nav>
   );
 }
 
