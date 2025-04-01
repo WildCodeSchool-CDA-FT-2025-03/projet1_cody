eea2060c969cc582dae4caa11928302a18f82598
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-04-01 15:01:42 +0200

    [UPD] Finalisation première table game

--- server/src/graphql/game/game.entities.ts
+++ server/src/graphql/game/game.entities.ts
@@ -1,49 +1,66 @@
+import { Field, ObjectType } from 'type-graphql';
 import { Column, Entity, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
 
+@ObjectType()
 @Entity()
 export class Game extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field()
   id: number;
 
   @Column()
+  @Field()
   title: string;
 
   @Column()
+  @Field()
   subtitle: string;
 
   @Column()
+  @Field()
   release_date: Date;
 
   @Column()
+  @Field()
   ISBN: string;
 
   @Column()
+  @Field()
   format: string;
 
   @Column()
+  @Field()
   duration: string;
 
   @Column()
+  @Field()
   summary: string;
 
   @Column()
+  @Field()
   target_audience: string;
 
   @Column()
+  @Field()
   original_language: string;
 
   @Column()
+  @Field()
   series: boolean;
 
   @Column()
+  @Field()
   extract: string;
 
   @Column()
+  @Field()
   game_engine: string;
 
   @Column()
+  @Field()
   PEGI_ESRB_rating: string;
 
   @Column()
+  @Field()
   mod_support: string;
 }
--- server/src/graphql/game/game.resolvers.ts
+++ server/src/graphql/game/game.resolvers.ts
@@ -4,9 +4,7 @@ import { Resolver, Query } from "type-graphql";
 @Resolver(Game)
 export default class GameResolvers {
   @Query(()=> [Game])
-  async getCartoons(): Promise<Game[]> {
-    return await Game.find({
-      relations : ["genres", "personnages"],
-    });
+  async getGames(): Promise<Game[]> {
+    return await Game.find();
   };
 }
