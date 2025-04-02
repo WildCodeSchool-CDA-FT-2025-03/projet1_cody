8541c3a7961f8d78d9cdc6f08a4aa78bd496f1b5
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-02 09:23:12 +0200

    Rectification de main

--- client/src/pages/BookPage/BookPage.tsx
+++ client/src/pages/BookPage/BookPage.tsx
@@ -1,25 +1,13 @@
-import NavRoot from "../../components/NavRoot/NavRoot";
 import BookRoot from "../../components/BookRoot/BookRoot";
-import FooterRoot from "../../components/FooterRoot/FooterRoot";
 
 // Le style des balises HTML de toutes les racines de pages sont définit dans un seul module CSS
 import CSSTargetPage from "../StylePage.module.css";
 
 function BookPage() {
   return (
-    <div className={CSSTargetPage.ContainerPage}>
-      <header className={CSSTargetPage.Header}>
-        <NavRoot />
-      </header>
-
-      <main className={CSSTargetPage.Main}>
-        <BookRoot />
-      </main>
-
-      <div className={CSSTargetPage.Footer}>
-        <FooterRoot />
-      </div>
-    </div>
+    <main className={CSSTargetPage.Main}>
+      <BookRoot />
+    </main>
   );
 }
 
