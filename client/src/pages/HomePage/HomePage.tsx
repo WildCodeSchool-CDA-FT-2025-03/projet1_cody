5fda5d082331a530aac215131ea63b7ad2bbb4e9
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-02 15:27:56 +0200

    Test

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-import HomeRoot from "../../components/HomeRoot/HomeRoot";
 
 // Le style des balises HTML de toutes les racines de pages sont définit dans un seul module CSS
 import CSSTargetPage from "../StylePage.module.css";
@@ -6,7 +5,6 @@ import CSSTargetPage from "../StylePage.module.css";
 function HomePage() {
   return (
     <main className={CSSTargetPage.Main}>
-      <HomeRoot />
     </main>
   );
 }
