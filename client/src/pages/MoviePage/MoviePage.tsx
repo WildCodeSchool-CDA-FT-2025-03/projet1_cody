4f40a1362b265bf3226e092c7c2da9df5d7e5533
Yiroma <yiromaric@gmail.com>
2025-04-03 12:40:29 +0200

    [FEAT] début de création de la page film

--- client/src/pages/MoviePage/MoviePage.tsx
+++ client/src/pages/MoviePage/MoviePage.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import routerClient from "../../router/Router";
+
+import arrow from "../../assets/icons/arrow.svg";
+import styles from "./MoviePage.module.css";
 
 function MoviePage() {
   return (
     <>
-      <div>
-        <h1>MoviePage</h1>
-      </div>
+      <section className={styles.moviePage}>
+        <div className={styles.moviePageHeader}>
+          <Link className={styles.arrow} to={`${routerClient[0].path}`}>
+            <img src={arrow} alt="retour vers l'accueil" />
+          </Link>
+          <h2>Films</h2>
+        </div>
+      </section>
     </>
   );
 }
