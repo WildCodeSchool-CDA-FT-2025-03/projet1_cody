67b5114522183213e59e57799c3cbb87ebf991ea
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-04-03 17:01:30 +0200

    [FIX] Chngement nom tables

--- server/src/graphql/game/game.resolvers.ts
+++ server/src/graphql/game/game.resolvers.ts
@@ -1,8 +1,15 @@
 import { Game } from "./game.entities";
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Arg } from "type-graphql";
 
 @Resolver(Game)
 export default class GameResolvers {
+  @Query(() => Game, { nullable: true })
+  async getOneGameById(@Arg("id") id: string): Promise<Game | null> {
+    return await Game.findOne({
+      where: { id: parseInt(id) },
+    });
+  }
+
   @Query(() => [Game])
   async getGames(): Promise<Game[]> {
     return await Game.find();
