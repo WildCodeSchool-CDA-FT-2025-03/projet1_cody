4817bbe6c9fcb12999fd9196fb3b28c0f5f07554
Yiroma <yiromaric@gmail.com>
2025-04-04 11:51:54 +0200

    [FEAT] afficher les cards dans content page

--- client/src/components/ContentList/ContentList.tsx
+++ client/src/components/ContentList/ContentList.tsx
@@ -1,15 +1,30 @@
-import { ContentType } from "../../types/ContentType";
+import CardRoot from "../ComponentsRoot/CardRoot";
+import CardDataType from "../../types/Card.type";
+
+import Music from "../../assets/images/music.jpeg";
+import Avenger from "../../assets/images/Avenger.jpg";
 
 import styles from "./ContentList.module.css";
 
-type ContentListProps = {
-  contentType: ContentType;
-};
+const fakeData: CardDataType[] = [
+  { id: 1, title: "Avenger", image: Music, alt: "Affiche Avenger", year: 2012 },
+  { id: 2, title: "Titanic", image: Avenger, alt: "Affiche Titanic", year: 1997 },
+  { id: 3, title: "Inception", image: Avenger, alt: "Affiche Inception", year: 2010 },
+  { id: 4, title: "Batman", image: Avenger, alt: "Affiche Batman", year: 2008 },
+  { id: 5, title: "Spiderman", image: Avenger, alt: "Affiche Spiderman", year: 2021 },
+  { id: 1, title: "Avenger", image: Music, alt: "Affiche Avenger", year: 2012 },
+  { id: 2, title: "Titanic", image: Avenger, alt: "Affiche Titanic", year: 1997 },
+  { id: 3, title: "Inception", image: Avenger, alt: "Affiche Inception", year: 2010 },
+  { id: 4, title: "Batman", image: Avenger, alt: "Affiche Batman", year: 2008 },
+  { id: 5, title: "Spiderman", image: Avenger, alt: "Affiche Spiderman", year: 2021 },
+];
 
-function ContentList({ contentType }: ContentListProps) {
+function ContentList() {
   return (
     <div className={styles.contentList}>
-      <span>Card à insérer ici pour {contentType}</span>
+      {fakeData.map((card) => (
+        <CardRoot key={card.id} {...card} />
+      ))}
     </div>
   );
 }
