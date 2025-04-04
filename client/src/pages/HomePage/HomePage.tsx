f730a3067e6da894fd1281da4e7dadb8c5e5b299
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-04 11:52:37 +0200

    Rectification et adaptation des tailles de police titre et year des cards

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -5,8 +5,8 @@ import CSSTargetPage from "../StylePage.module.css";
 
 const fakeData = [
   { id: 1, title: "Avenger", image: Music, alt: "Affiche Avenger", year: 2012 },
-  { id: 2, title: "Titanic", image: Avenger, alt: "Affiche Titanic", year: 1997 },
-  { id: 3, title: "Inception", image: Avenger, alt: "Affiche Inception", year: 2010 },
+  { id: 2, title: "Le Seigneur des Anneaux", image: Avenger, alt: "Affiche Titanic", year: 1997 },
+  { id: 3, title: "Astérix et Obélix : au service de Sa Majesté", image: Avenger, alt: "Affiche Inception", year: 2010 },
   { id: 4, title: "Batman", image: Avenger, alt: "Affiche Batman", year: 2008 },
   { id: 5, title: "Spiderman", image: Avenger, alt: "Affiche Spiderman", year: 2021 },
   { id: 1, title: "Avenger", image: Music, alt: "Affiche Avenger", year: 2012 },
