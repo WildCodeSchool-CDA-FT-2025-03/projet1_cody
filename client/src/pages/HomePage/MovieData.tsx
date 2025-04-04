103b288652855ac46f0741f4d28b19f8d1138485
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-04 12:21:52 +0200

    Intégration d'image

---
+++ client/src/pages/HomePage/MovieData.tsx
@@ -0,0 +1,95 @@
+import CardDataType from "../../types/Card.type";
+
+import image1 from "../../assets/images/inception.jpg";
+import image2 from "../../assets/images/le-parrain.jpg";
+import image3 from "../../assets/images/la-la-land.webp";
+import image4 from "../../assets/images/le-voyage-de-chihiro.jpg";
+import image5 from "../../assets/images/parasite.webp";
+import image6 from "../../assets/images/the-dark-knight.jpg";
+import image7 from "../../assets/images/amelie.webp";
+import image8 from "../../assets/images/avatar.webp";
+import image9 from "../../assets/images/intouchables.jpg";
+import image10 from "../../assets/images/le-seigneur-des-anneaux.jpg";
+import image11 from "../../assets/images/le-roi-lion.webp";
+
+const MovieData: CardDataType[] = [
+  {
+    id: 1,
+    image: image1,
+    title: "Inception",
+    alt: "Inception",
+    year: 2010,
+  },
+  {
+    id: 2,
+    image: image2,
+    title: "Le Parrain",
+    alt: "Le Parrain",
+    year: 1972,
+  },
+  {
+    id: 3,
+    image: image3,
+    title: "La La Land",
+    alt: "La La Land",
+    year: 2016,
+  },
+  {
+    id: 4,
+    image: image4,
+    title: "Le Voyage de Chihiro",
+    alt: "Le Voyage de Chihiro",
+    year: 2001,
+  },
+  {
+    id: 5,
+    image: image5,
+    title: "Parasite",
+    alt: "Parasite",
+    year: 2019,
+  },
+  {
+    id: 6,
+    image: image6,
+    title: "The Dark Knight",
+    alt: "The Dark Knight",
+    year: 2008,
+  },
+  {
+    id: 7,
+    image: image7,
+    title: "Amélie Poulain",
+    alt: "Amélie Poulain",
+    year: 2001,
+  },
+  {
+    id: 8,
+    image: image8,
+    title: "Avatar",
+    alt: "Avatar",
+    year: 2009,
+  },
+  {
+    id: 9,
+    image: image9,
+    title: "Intouchables",
+    alt: "Intouchables",
+    year: 2011,
+  },
+  {
+    id: 10,
+    image: image10,
+    title: "Le Seigneur des Anneaux : La Communauté de l'Anneau",
+    alt: "Le Seigneur des Anneaux : La Communauté de l'Anneau",
+    year :2001
+  },
+  {
+    id :11 ,
+    image :image11 ,
+    title :"Le Roi Lion" ,
+    alt :"Le Roi Lion" ,
+    year :1994
+  }
+];
+
+export default MovieData;
