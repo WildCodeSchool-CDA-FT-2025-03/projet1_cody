f494f42f072818171d6be5968c571516120180a8
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-04-10 00:14:55 +0200

    [FEAT] Supression ligne morte

--- client/src/components/ContentList/ContentList.tsx
+++ client/src/components/ContentList/ContentList.tsx
@@ -63,11 +63,7 @@ function ContentList({
         setSortedContent(contentToDisplay?.sort((a, b) => a.title.localeCompare(b.title)));
       } else if (sortOption === "alphabetical-reverse") {
         setSortedContent(contentToDisplay?.sort((a, b) => b.title.localeCompare(a.title)));
-      } /*else if (sortOption === "date-recent") {
-        setSortedContent(contentToDisplay?.sort((a, b) => b.year - a.year));
-      } else if (sortOption === "date-old") {
-        setSortedContent(contentToDisplay?.sort((a, b) => a.year - b.year));
-      } */else {
+      } else {
         setSortedContent(contentToDisplay);
       }
     }
