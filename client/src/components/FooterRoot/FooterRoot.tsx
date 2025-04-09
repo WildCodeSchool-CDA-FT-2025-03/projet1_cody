f2b56187f382a93292a74e714129f6c33860af56
Yiroma <yiromaric@gmail.com>
2025-04-09 22:32:37 +0200

    [REFACTOR] déplacement de newsletter dans un composant

--- client/src/components/FooterRoot/FooterRoot.tsx
+++ client/src/components/FooterRoot/FooterRoot.tsx
@@ -1,23 +1,16 @@
 import { Link } from "react-router-dom";
-import style from "./FooterRoot.module.css";
+
+import Newsletter from "../../components/Newsletter/Newsletter";
+
 import logo from "../../assets/icons/logo-hive.svg";
 import logoText from "../../assets/icons/logo-hive-text.svg";
+
+import style from "./FooterRoot.module.css";
 
 function FooterRoot() {
   return (
     <footer className={style.FooterRoot}>
-      <div className={style.NewsletterContainer}>
-        <h3>Je m'abonne à la newsletter pour être informer des dernières nouveautés</h3>
-        <form className={style.newsletterForm}>
-          <label htmlFor="email" className={style.visuallyHidden}>
-            Email
-          </label>
-          <input type="email" placeholder="Votre email" className={style.newsletterInput} />
-          <button type="submit" className={style.newsletterButton}>
-            S'abonner
-          </button>
-        </form>
-      </div>
+      <Newsletter />
       <Link to="/" className={style.logoLink}>
         <img className={style.logoIconNav} src={logo} alt="Logo Hive" />
         <img className={style.logoTextNav} src={logoText} alt="Logo Hive texte" />
