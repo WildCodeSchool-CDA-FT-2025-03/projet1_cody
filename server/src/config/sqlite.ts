c18a793255f137e3ff4dcef2011d8e37d16e23c1
Yiroma <yiromaric@gmail.com>
2025-04-09 11:29:30 +0200

    [FIX] correction du fichier sqlite

--- server/src/config/sqlite.ts
+++ server/src/config/sqlite.ts
@@ -13,10 +13,15 @@ import { MovieCategory } from "../graphql/movie/movie_category.entities";
 // Book
 import { Book } from "../graphql/book/book.entities";
 
+// Music
+import { Album } from "../graphql/music/album.entities";
+import { Track } from "../graphql/music/track.entities";
+import { Artist } from "../graphql/music/artist.entities";
+import { AlbumCategory } from "../graphql/music/album_category.entities";
+
 import "dotenv/config";
 
 const isDev = process.env.ENVIRONNEMENT === "development";
-
 export const dataSource = new DataSource({
   type: "sqlite", // Type de BDD recherchée (MySQL, PostGres, SQLite, ...)
   database: "./db.sqlite", // Prépférable de la mettre en .env
@@ -28,9 +33,13 @@ export const dataSource = new DataSource({
     GameAward,
     GameCategory,
     Platform,
-    MovieAward,
     MovieActors,
+    MovieAward,
     MovieCategory,
+    Album,
+    Track,
+    Artist,
+    AlbumCategory,
   ], // On placera nos modèles de données ici
   synchronize: isDev, // Propriété de stratégie de synchronisation (ici, l'on synchronise à chaque fois que l'on lance le projet) ! Ne pas laisser en prod
   logging: isDev,
