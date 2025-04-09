6f102795a542d71c2ac1e6ff6ca1a8d8445ab99c
Yiroma <yiromaric@gmail.com>
2025-04-09 23:19:56 +0200

    [FEAT] finalisation du footer avec style css et responsive

--- client/src/components/FooterRoot/FooterRoot.tsx
+++ client/src/components/FooterRoot/FooterRoot.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import routerClient from "../../router/Router";
 
 import Newsletter from "../../components/Newsletter/Newsletter";
 
@@ -7,19 +8,57 @@ import logoText from "../../assets/icons/logo-hive-text.svg";
 
 import style from "./FooterRoot.module.css";
 
+const navItems = [
+  { text: "Accueil", path: "/" },
+  { text: "Films", path: routerClient[2].path },
+  { text: "Musiques", path: routerClient[3].path },
+  { text: "Jeux", path: routerClient[4].path },
+  { text: "Livres", path: routerClient[5].path },
+];
+
+const creditsItems = [
+  { name: "Alexandre Dumout", path: "https://www.linkedin.com/in/alexandre-dumout-317505123/" },
+  { name: "Romaric YI", path: "https://www.linkedin.com/in/yiromaric/" },
+  { name: "Ryan Decian", path: "https://www.linkedin.com/in/ryan-decian-864696302/" },
+];
+
 function FooterRoot() {
   return (
     <footer className={style.FooterRoot}>
       <Newsletter />
-      <Link to="/" className={style.logoLink}>
-        <img className={style.logoIconNav} src={logo} alt="Logo Hive" />
-        <img className={style.logoTextNav} src={logoText} alt="Logo Hive texte" />
-      </Link>
-      <span>Home media center communautaire</span>
-      <span>
-        Enregistrez votre collection et partagez vos critiques de films, musiques, jeux vidéos et
-        livres
-      </span>
+      <div className={style.footerContent}>
+        <div className={style.footerContentLogo}>
+          <Link to="/" className={style.logoLink}>
+            <img className={style.logoIconNav} src={logo} alt="Logo Hive" />
+            <img className={style.logoTextNav} src={logoText} alt="Logo Hive texte" />
+          </Link>
+          <span>Home media center communautaire</span>
+          <span>
+            Enregistrez votre collection et partagez vos critiques de films, musiques, jeux vidéos
+            et livres
+          </span>
+        </div>
+
+        <nav>
+          <ul className={style.footerNav}>
+            {navItems.map((item) => (
+              <li key={item.text}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      <ul className={style.footerCredits}>
+        {creditsItems.map((item) => (
+          <li key={item.name}>
+            <a href={item.path} target="_blank">
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 }
