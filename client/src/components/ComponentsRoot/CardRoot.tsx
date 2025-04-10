c6edf66fbad5a3157701e9e9b59044b4572c3ec9
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-04-10 22:59:46 +0200

    [FEAT] Ajout Link sur card

--- client/src/components/ComponentsRoot/CardRoot.tsx
+++ client/src/components/ComponentsRoot/CardRoot.tsx
@@ -1,14 +1,32 @@
 import style from "./CardRoot.module.css";
 import CardDataType from "../../types/Old-card.type";
+import { Link } from "react-router-dom";
 
-function CardRoot({ title, image_url, image_alt }: CardDataType) {
+type CardRootProps = CardDataType & {
+  contenttype: string;
+};
+
+function CardRoot({ id, title, image_url, image_alt, contenttype }: CardRootProps ) {
+  let pageDetailLink = "movie";
+  switch (contenttype) {
+  case "jeux":
+    pageDetailLink = "game";
+    break;
+  case "musique":
+    pageDetailLink = "music";
+    break;
+  }
   return (
     <article className={style.CardRoot}>
       <figure className={style.ContainerImage}>
-        <img src={image_url} alt={image_alt} />
+        <Link to={"/" + pageDetailLink + "/" + id  }>
+          <img src={image_url} alt={image_alt} />
+        </Link>
       </figure>
       <div className={style.ContainerText}>
-        <h1 className={style.Title}>{title}</h1>
+        <Link to={"/" + pageDetailLink + "/" + id  }>
+          <h1 className={style.Title}>{title}</h1>
+        </Link>
       </div>
     </article>
   );
