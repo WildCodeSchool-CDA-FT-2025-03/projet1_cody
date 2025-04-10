1202385c2a44e46b90ea62ce874b7a6862db26d3
Yi Romaric <119810671+Yiroma@users.noreply.github.com>
2025-04-10 17:31:35 +0200

    Update AddMediaPage.tsx

--- client/src/pages/AddMediaPage/AddMediaPage.tsx
+++ client/src/pages/AddMediaPage/AddMediaPage.tsx
@@ -5,11 +5,9 @@ import styles from "./AddMediaPage.module.css";
 
 function AddMediaPage() {
   return (
-    <>
-      <section className={styles.addMediaPage}>
-        <TitleAndBtnReturn title="Ajouter un média" />
-      </section>
-    </>
+    <section className={styles.addMediaPage}>
+      <TitleAndBtnReturn title="Ajouter un média" />
+    </section>
   );
 }
 
