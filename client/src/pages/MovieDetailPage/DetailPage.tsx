ccff9940deba43e9cdee7ab63548d25e12848223
Alexandre DUMOUT <alexdum03@hotmail.fr>
2025-04-11 11:25:20 +0200

    [FEAT] Ajout page détail film

---
+++ client/src/pages/MovieDetailPage/DetailPage.tsx
@@ -0,0 +1,118 @@
+// Le style des balises HTML de toutes les racines de pages sont définit dans un seul module CSS
+import CSSTargetPage from "./DetailPage.module.css";
+import {
+  gql,
+} from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import DetailRoot from "../../components/DetailRoot/DetailRoot";
+import SpecificField from "../../components/SpecificField/SpecificField";
+import CardDataType from "../../types/Old-card.type";
+import CarrouselRoot from "../../components/ComponentsRoot/CarrouselRoot";
+import TitleAndBtnReturn from "../../components/ContentTitleAndBtnReturn/ContentTitleAndBtnReturn";
+
+const GET_ALL_MOVIE = gql`
+  query GetOneMovieById($id: String!) {
+  getOneMovieById(id: $id) {
+    title
+    duration
+    summary
+    producers
+    writers
+    studios
+    image_alt
+    image_url
+    movie_awards {
+      name
+    }
+    movie_categories {
+      name
+    }
+    release_date
+    targeted_audience
+  }
+  getLastMovies {
+    id
+    image_alt
+    image_url
+    title
+  }
+}
+`;
+
+type getOneMovie = {
+  getLastMovies: CardDataType[];
+  getOneMovieById: {
+    title: string;
+    duration: number;
+    summary: string;
+    producers: string;
+    writers: string;
+    studios: string;
+    image_url: string;
+    image_alt: string;
+    movie_awards: {
+      name: string;
+    }[];
+    movie_categories: {
+      name: string;
+    }[];
+    targeted_audience: string;
+    release_date: Date;
+  };
+};
+
+const specificField = ["producers", "writers", "studios"];
+function MovieDetailPage() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery<getOneMovie>(GET_ALL_MOVIE, {
+    variables: { id: id },
+  });
+  const myawards : string[] = [];
+  const mycategory : string[] = [];
+
+  if (loading) return <p>Loading in progress...</p>;
+  if (error) return <p>There might be an issue</p>;
+
+  data?.getOneMovieById.movie_awards.forEach((a,index) => {
+    myawards[index] = a.name;
+  });
+  data?.getOneMovieById.movie_categories.forEach((c,index) => {
+    mycategory[index] = c.name;
+  });
+
+  const getField = (data: getOneMovie | undefined, specificField : string[])  => {
+    const result: Record<string, string> = {};
+    specificField.forEach(fd => {
+      result[fd] = data?.getOneMovieById[fd as keyof typeof data.getOneMovieById] as string;
+    });
+    return result;
+  };
+
+  let yeartMovie = "";
+  if(data?.getOneMovieById.release_date) {
+    yeartMovie = new Date(data?.getOneMovieById.release_date).getFullYear().toString();
+  }
+
+  return (
+    <main className={CSSTargetPage.Main}>
+      <TitleAndBtnReturn title="Détail film" />
+      <div className={CSSTargetPage.container}>
+        <DetailRoot 
+          image_url={data?.getOneMovieById.image_url || ""} 
+          image_alt={data?.getOneMovieById.image_alt || ""} 
+          pegi_esbr_rating={data?.getOneMovieById.targeted_audience || ""} 
+          title={data?.getOneMovieById.title || ""} 
+          year={yeartMovie} 
+          duration={data?.getOneMovieById.duration || 0} 
+          summary={data?.getOneMovieById.summary || ""} 
+          awards={myawards} 
+          category={mycategory}/>
+        <SpecificField data={getField(data, specificField)} />
+      </div>
+      <CarrouselRoot cards={data?.getLastMovies} h2="film"/>
+    </main>
+  );
+}
+
+export default MovieDetailPage;
--- server/src/graphql/movie/movie.resolvers.ts
+++ server/src/graphql/movie/movie.resolvers.ts
@@ -1,5 +1,5 @@
 import { Movie } from "./movie.entities";
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Arg } from "type-graphql";
 
 @Resolver(Movie)
 export default class MovieResolvers {
@@ -16,5 +16,19 @@ export default class MovieResolvers {
       },
       take: 15,
     });
+  }
+
+  @Query(() => Movie, { nullable: true })
+  async getOneMovieById(@Arg("id") id: string): Promise<Movie | null> {
+    if (!isNaN(parseInt(id))) {
+      return await Movie.findOne({
+        where: { id: parseInt(id) },
+        relations: {
+          movie_awards: true,
+          movie_categories: true,
+        },
+      });
+    }
+    return null;
   }
 }
